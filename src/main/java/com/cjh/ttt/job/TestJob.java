5bcbe0a67fc80db71eb9689b350ff05b043bb83d
wxgitcjh <1109551489@qq.com>
2020-04-04 20:47:38 +0800

    优化日志

--- src/main/java/com/cjh/ttt/job/TestJob.java
+++ src/main/java/com/cjh/ttt/job/TestJob.java
@@ -2,8 +2,6 @@ package com.cjh.ttt.job;
 
 import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,21 +12,14 @@ import org.springframework.stereotype.Component;
  * @author cjh
  * @date 2020/3/31
  */
-@RefreshScope
 @Component
 @EnableScheduling
 @Slf4j
 public class TestJob {
 
-
-    @Value("${job.test.enable}")
-    private Boolean testEnable;
-
-    @Scheduled(cron = "${job.test.cron}")
+    @Scheduled(cron = "${job.test}")
     public void test() {
-        if (testEnable) {
-            System.out.println("test job: " + new Date());
-        }
+        System.out.println("test job: " + new Date());
     }
 
 }
